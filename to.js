b9231b851bf39e0484f282f156f9332cbc33ce94
Samyahmed480 <152093199+Samyahmed480@users.noreply.github.com>
2025-09-12 00:01:40 +0300

    Add files via upload

---
+++ to.js
@@ -0,0 +1,14 @@
+
+function validateCode(event) {
+    event.preventDefault();
+    const code = document.getElementById('code').value;
+    const validCode = "1234";
+
+    if (code === validCode) {
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'https://egypt-academy.github.io/uc/basics-course.html';
+    } else {
+        alert('كود غير صحيح');
+    }
+    return false;
+}
